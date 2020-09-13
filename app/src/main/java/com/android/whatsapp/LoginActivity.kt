11a317d79e76d8224d9ca7f8c0d386c86a841cbe
Anish <anishanand3733@gmail.com>
2020-09-13 19:33:43 +0530

    Register user and resend token

---
+++ app/src/main/java/com/android/whatsapp/LoginActivity.kt
@@ -0,0 +1,88 @@
+package com.android.whatsapp
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.credentials.Credential
+import com.google.android.gms.auth.api.credentials.Credentials
+import com.google.android.gms.auth.api.credentials.HintRequest
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var phoneNumber: String
+    private lateinit var countryCode: String
+    private val CREDENTIAL_PICKER_REQUEST = 1  // Set to an unused request code
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //requestHint()
+
+        phoneNumberEt.addTextChangedListener {
+            nextBtn.isEnabled = !(it.isNullOrEmpty() || it.length < 10)
+        }
+
+        nextBtn.setOnClickListener {
+            checkNumber()
+        }
+    }
+
+//    // Construct a request for phone numbers and show the picker
+//    private fun requestHint() {
+//        val hintRequest = HintRequest.Builder()
+//            .setPhoneNumberIdentifierSupported(true)
+//            .build()
+//        val credentialsClient = Credentials.getClient(this)
+//        val intent = credentialsClient.getHintPickerIntent(hintRequest)
+//        startIntentSenderForResult(
+//            intent.intentSender,
+//            CREDENTIAL_PICKER_REQUEST,
+//            null, 0, 0,0
+//        )
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        when(requestCode) {
+//            CREDENTIAL_PICKER_REQUEST ->
+//                if(resultCode == Activity.RESULT_OK && data != null) {
+//                    val credential = data.getParcelableExtra<Credential>(Credential.EXTRA_KEY)
+//                }
+//        }
+//    }
+
+    private fun checkNumber() {
+        countryCode = ccp.selectedCountryCodeWithPlus
+        phoneNumber = countryCode + phoneNumberEt.text.toString()
+
+        notifyUser()
+    }
+
+    private fun notifyUser() {
+        MaterialAlertDialogBuilder(this).apply {
+            setMessage("We will be verifying the phone number:$phoneNumber\n" +
+                    "Is this OK, or would you like to edit the number?")
+            setPositiveButton("Ok") { _,_ ->
+                showOtpActivity()
+            }
+            setNegativeButton("Edit") {dialog, which ->
+                dialog.dismiss()
+            }
+            setCancelable(false)
+            create()
+            show()
+        }
+    }
+
+    private fun showOtpActivity() {
+        startActivity(Intent(this, OtpActivity::class.java).putExtra(PHONE_NUMBER,phoneNumber))
+        finish()
+    }
+}
