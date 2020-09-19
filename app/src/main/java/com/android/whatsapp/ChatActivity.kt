b2d13f5b55b673e4ba93321995ce0b05db6d92d0
Anish <anishanand3733@gmail.com>
2020-09-19 14:42:08 +0530

    Update inbox with unread count

--- app/src/main/java/com/android/whatsapp/ChatActivity.kt
+++ app/src/main/java/com/android/whatsapp/ChatActivity.kt
@@ -2,10 +2,15 @@ package com.android.whatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.whatsapp.models.Inbox
 import com.android.whatsapp.models.Message
 import com.android.whatsapp.models.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 import com.vanniktech.emoji.EmojiManager
@@ -18,13 +23,13 @@ const val IMAGE = "photo"
 
 class ChatActivity : AppCompatActivity() {
 
-    private val friendId:String by lazy {
+    private val friendId: String by lazy {
         intent.getStringExtra(UID)
     }
-    private val name:String by lazy {
+    private val name: String by lazy {
         intent.getStringExtra(NAME)
     }
-    private val image:String by lazy {
+    private val image: String by lazy {
         intent.getStringExtra(IMAGE)
     }
     private val mCurrentUid: String by lazy {
@@ -50,7 +55,7 @@ class ChatActivity : AppCompatActivity() {
 
         sendBtn.setOnClickListener {
             msgEdtv.text?.let {
-                if(it.isNotEmpty()) {
+                if (it.isNotEmpty()) {
                     sendMessage(it.toString())
                     it.clear()
                 }
@@ -61,22 +66,61 @@ class ChatActivity : AppCompatActivity() {
     private fun sendMessage(msg: String) {
         val id = getMessages(friendId).push().key  //uniquekey
         checkNotNull(id) { "Cannot be null" }
-        val msgMap = Message(msg,mCurrentUid,id)
+        val msgMap = Message(msg, mCurrentUid, id)
         getMessages(friendId).child(id).setValue(msgMap).addOnSuccessListener {
-
+            Log.i("CHATS", "Completed")
         }.addOnFailureListener {
-
+            Log.i("CHATS", it.localizedMessage)
         }
+        updateLastMessage(msgMap)
+    }
+
+    private fun updateLastMessage(message: Message) {
+        val inboxMap = Inbox(
+            message.msg,
+            friendId,
+            name,
+            image,
+            count = 0
+        )
+
+        getInbox(mCurrentUid, friendId).setValue(inboxMap).addOnSuccessListener {
+            getInbox(friendId, mCurrentUid).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val value = snapshot.getValue(Inbox::class.java)
+
+                    inboxMap.apply {
+                        from = message.senderId
+                        name = currentUser.name
+                        image = currentUser.thumbImage
+                        count = 1
+                    }
+                    value?.let {
+                        if (it.from == message.senderId) {
+                            inboxMap.count = value.count + 1
+                        }
+                    }
+                    getInbox(friendId, mCurrentUid).setValue(inboxMap)
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
+        }
+    }
+
+    private fun markAsRead() {
+        getInbox(friendId,mCurrentUid).child("count").setValue(0)
     }
 
     private fun getMessages(friendId: String) =
         db.reference.child("messages/${getId(friendId)}")
 
-    private fun getInbox(toUser: String, fromUser:String) =
+    private fun getInbox(toUser: String, fromUser: String) =
         db.reference.child("chats/$toUser/$fromUser")
 
     private fun getId(friendId: String): String { //get Id for the messages
-        return if(friendId > mCurrentUid) {
+        return if (friendId > mCurrentUid) {
             mCurrentUid + friendId
         } else {
             friendId + mCurrentUid
